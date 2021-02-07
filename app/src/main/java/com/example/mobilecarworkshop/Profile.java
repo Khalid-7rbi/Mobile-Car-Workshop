be896e3cb2b7d427f5423ed64af61432eea69252
khalid-7rbi <khaledalhrbe8@gmail.com>
2021-02-07 04:46:41 +0300

    Store User profile information

---
+++ app/src/main/java/com/example/mobilecarworkshop/Profile.java
@@ -0,0 +1,54 @@
+package com.example.mobilecarworkshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import javax.annotation.Nullable;
+
+public class Profile extends AppCompatActivity {
+
+
+
+    TextView fullName, email,phone;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        fullName = findViewById(R.id.nameProfile);
+        email    = findViewById(R.id.emailProfile);
+        phone    = findViewById(R.id.phoneProfile);
+
+        fAuth    = FirebaseAuth.getInstance();
+        fStore   = FirebaseFirestore.getInstance();
+
+        userID   = fAuth.getCurrentUser().getUid();
+
+        final DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                phone.setText(documentSnapshot.getString("phone"));
+                email.setText(documentSnapshot.getString("email"));
+                fullName.setText(documentSnapshot.getString("fName"));
+            }
+        });
+
+
+
+    }
+}
--- app/src/main/java/com/example/mobilecarworkshop/Regeister.java
+++ app/src/main/java/com/example/mobilecarworkshop/Regeister.java
@@ -1,10 +1,12 @@
 package com.example.mobilecarworkshop;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,17 +15,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Regeister extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     EditText mFullName,mEmail,mPassword,mPhone;
     Button mRegisterdBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore  fStore;
+    String userID;
 
 
     @Override
@@ -39,7 +50,10 @@ public class Regeister extends AppCompatActivity {
         mLoginBtn    = findViewById(R.id.CreateText);
 
         fAuth        = FirebaseAuth.getInstance();
+        fStore       = FirebaseFirestore.getInstance();
         progressBar  = findViewById(R.id.progressBar);
+
+
 
         if(fAuth.getCurrentUser()!=null){
 
@@ -50,8 +64,10 @@ public class Regeister extends AppCompatActivity {
         mRegisterdBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+                final String phone    = mPhone.getText().toString();
 
                 if(TextUtils.isEmpty(email)){
 
@@ -82,6 +98,18 @@ public class Regeister extends AppCompatActivity {
                         if(task.isSuccessful()){
 
                             Toast.makeText(Regeister.this, "User Created ", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fName",fullName);
+                            user.put("email",email);
+                            user.put("phone",phone);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "onSuccess: user profile is created "+userID);
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), Customer.class));
                         }
                         else{
