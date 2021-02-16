a74e062b3d85586ef27cecad8999f68f2c0c504d
mzaidan213 <78346404+mzaidan213@users.noreply.github.com>
2021-02-16 11:52:26 +0300

    support addition2

---
+++ app/src/main/java/com/example/mobilecarworkshop/support.java
@@ -0,0 +1,26 @@
+package com.example.mobilecarworkshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class support extends AppCompatActivity {
+
+    private TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_support);
+        Button send = (Button)findViewById(R.id.send);
+        txtView=(TextView)findViewById(R.id.sent);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtView.setText("message sent");
+            }
+        });
+    }
+}
