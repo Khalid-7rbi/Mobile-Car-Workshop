119d2907fbc1db15dd8112cf9839719942426997
mzaidan213 <78346404+mzaidan213@users.noreply.github.com>
2021-02-27 23:24:53 +0300

    workshop main

--- app/src/main/java/com/example/mobilecarworkshop/Workshop_main.java
+++ app/src/main/java/com/example/mobilecarworkshop/Workshop_main.java
@@ -2,7 +2,11 @@ package com.example.mobilecarworkshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Workshop_main extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class Workshop_main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workshop_main);
+
+
+    }
+    public void info(View view){
+
+        startActivity(new Intent(getApplicationContext(),info.class));
+
+    }
+
+    public void Available(View view){
+
+        startActivity(new Intent(getApplicationContext(),Available.class));
+
+    }
+
+    public void logoutW(View view){
+
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Workshop_login.class));
+        finish();
+
     }
 }
